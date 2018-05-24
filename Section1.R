35fd8f9e9210a7a501c61ac4eb213c124d62e775
MiriamSlattery <miriamslattery11@gmail.com>
2018-05-24 15:00:12 +0930

    Here

--- Section1.R
+++ Section1.R
@@ -1,7 +1,7 @@
 #Entering data
-Child<- c(1:12) #🤱
-Height<-c(108.7,161.29,95.25,100.33,115.57,97.79,109.22,57.15,93.98,59.69,83.82,147.32) #📏
-Weight<-c(18.14,42.41,16.10,13.61,23.59,7.71,17.46,3.86,14.97,4.31,9.53,35.83) #🍔
+Child<- c(1:12)
+Height<-c(108.7,161.29,95.25,100.33,115.57,97.79,109.22,57.15,93.98,59.69,83.82,147.32)
+Weight<-c(18.14,42.41,16.10,13.61,23.59,7.71,17.46,3.86,14.97,4.31,9.53,35.83)
 Length<-c(37,49.5,34.5,36,43,28,37,20,33.5,30.5,38.5,47.0)
 catheter <-data.frame(Child, Height, Weight, Length)
 head(catheter)
@@ -27,7 +27,13 @@ res1<-residuals(lm1)
 plot(catheter$Height, res1)
 plot(catheter$Weight, res1)
 
-
+#Talk about coeffcients
 summary(lm1)
 summary(lm2)
 summary(lm3)
+
+
+
+#Angle between subspaces
+theta<- acos((t(Height)%*%Weight)/(sqrt(sum(Height^2))*sqrt(sum(Weight^2))))
+theta
